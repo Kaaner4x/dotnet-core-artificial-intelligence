0deaf6320e1a4ffcc5488e3ffb237f58475013dc
Emirhan Peçe <kaaner.emirhan@gmail.com>
2026-09-10 08:18:28 +0300

    feat: EF Core entegrasyonu ve Müşteri CRUD API'si eklendi
    - WeatherForecast örnek kodları temizlendi
    - Customer varlığı ve ApiContext eklendi
    - Program.cs içerisine DbContext kaydı eklendi
    - CustomersController ile CRUD uç noktaları oluşturuldu
    - Müşteri tablosu için ilk migration dosyası eklendi

---
+++ DotnetCoreAI.Project01_ApiDemo/Context/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using DotnetCoreAI.Project01_ApiDemo.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetCoreAI.Project01_ApiDemo.Context
+{
+    public class ApiContext : DbContext
+    {
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;initial catalog=ApiAIDb; integrated security=true; trustServerCertificate=true;");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
