75d62bd5edb98d857ae09c783cac69e129694217
Emirhan Peçe <kaaner.emirhan@gmail.com>
2026-09-11 23:42:03 +0300

    feat: RapidAPI dizi listeleme projesi eklendi

---
+++ DotnetCoreAI.Project03_RapidApi/Program.cs
@@ -0,0 +1,26 @@
+﻿using DotnetCoreAI.Project03_RapidApi.ViewModels;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+var client = new HttpClient();
+List<ApiSeriesViewModel> apiSeriesViewModels = new List<ApiSeriesViewModel>();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+    Headers =
+    {
+        { "x-rapidapi-key", "5088c917d6msh6dbc7b8055deeedp198d06jsnaaddf6399d5f" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    apiSeriesViewModels = JsonConvert.DeserializeObject<List<ApiSeriesViewModel>>(body);
+    foreach(var series in apiSeriesViewModels)
+    {
+        Console.WriteLine($"Rank: {series.rank} - Title: {series.title} - Rating: {series.rating} - Year: {series.year}");
+    }
+}
+Console.ReadLine();
